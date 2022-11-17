089b66d30fc39c889b2c5dddf4cb2481a7e180be
LeArsiVU <arsi_vu@hotmail.com>
2022-11-17 12:39:01 -0700

    Se agrega funcion to_csv

--- example_isra.py
+++ example_isra.py
@@ -30,39 +30,47 @@ params: Iterable[dict] = ctl_dags.set_index('DAG', drop=False).to_dict('records'
 #Es posible definir más dags desde un solo archivo
 
 for param in params:
-	if param["Activo"] == True:
+    if param["Activo"] == True:
 
-		with DAG(
-		    param["DAG"],
-		    schedule=param["Schedule"],
-			start_date=pendulum.from_format(f'{param["Fecha Inicio"]}','YYYY-MM-DD', tz="America/Mazatlan"),
-		    catchup=False,
-		    dagrun_timeout=datetime.timedelta(minutes=4),
-			default_args={'owner':param['Owner'], 
-						  'retries':2,
-						  'retry_delay':timedelta(minutes=5)},
-			description= f"Actualiza  {param['Proyecto']}.{param['Dataset']}.{param['Tabla']}",
-		    tags=[param["Grupo"]]
-		) as dag:
-		    #Activar DAG
-		    #dag = DagModel.get_dagmodel(param["DAG"])
-		    #dag.set_is_paused(is_paused=False)
+        with DAG(
+            param["DAG"],
+            schedule=param["Schedule"],
+            start_date=pendulum.from_format(f'{param["Fecha Inicio"]}','YYYY-MM-DD', tz="America/Mazatlan"),
+            catchup=False,
+            dagrun_timeout=datetime.timedelta(minutes=4),
+            default_args={'owner':param['Owner'], 
+                          'retries':2,
+                          'retry_delay':timedelta(minutes=5)},
+            description= f"Actualiza  {param['Proyecto']}.{param['Dataset']}.{param['Tabla']}",
+            tags=[param["Grupo"]]
+        ) as dag:
+            #Activar DAG
+            #dag = DagModel.get_dagmodel(param["DAG"])
+            #dag.set_is_paused(is_paused=False)
 
-		    Tarea_1 = EmptyOperator(
-					task_id="Tarea_1",
-					trigger_rule="all_success",
-		    )			    
-		    Tarea_2 = EmptyOperator(
-					task_id="Tarea_2",
-					trigger_rule="all_success",
-		    )
-		    Tarea_3 = EmptyOperator(
-					task_id="Tarea_3",
-					trigger_rule="all_success",
-		    )
-		    Tarea_4 = EmptyOperator(
-					task_id="Tarea_4",
-					trigger_rule="all_success",
-		    )
-			    
-		Tarea_1>>[Tarea_2,Tarea_3]>>Tarea_4
+            @task(task_id="to_csv")
+            def to_csv(tabla):
+                csv = open(f"/home/isra/Descargas/{param['DAG']}.csv","a")
+                contenido = f"{tabla},{pendulum.now('America/Mazatlan')}"
+                csv.write(contenido)
+                return 0
+
+            Tarea_1 = EmptyOperator(
+                    task_id="Tarea_1",
+                    trigger_rule="all_success",
+            )			    
+            Tarea_2 = EmptyOperator(
+                    task_id="Tarea_2",
+                    trigger_rule="all_success",
+            )
+            Tarea_3 = EmptyOperator(
+                    task_id="Tarea_3",
+                    trigger_rule="all_success",
+            )
+            Tarea_4 = EmptyOperator(
+                    task_id="Tarea_4",
+                    trigger_rule="all_success",
+            )
+            Tarea_5 = to_csv(param['Tabla'])
+                
+        Tarea_1>>[Tarea_2,Tarea_3]>>Tarea_4>>Tarea_5
