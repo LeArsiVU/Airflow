493240bcbe624e65bd28fc4296459ca9fca35ad7
LeArsiVU <arsi_vu@hotmail.com>
2022-11-25 16:03:03 -0700

    Quitar linea comentada de start date

--- example_dag_postgresql_to_csv.py
+++ example_dag_postgresql_to_csv.py
@@ -89,7 +89,6 @@ for param in params:
             param["DAG"],
             schedule=param["Schedule"],
             start_date=pendulum.from_format(f'{param["Fecha Inicio"]}','YYYY-MM-DD', tz="America/Mazatlan"),
-            #start_date=pendulum.today('America/Mazatlan').add(days=-2),
             catchup=False,
             dagrun_timeout=datetime.timedelta(minutes=4),
             default_args={'owner':param['Owner'], 
